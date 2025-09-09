5639aeeec2f1981a032c96bfccf526052ecb290b
zaid <h.zaidamjad29@gmail.com>
2025-09-09 17:50:11 +0500

    put object in s3 bucket

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,4 +1,5 @@
-import { S3Client } from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { NextResponse } from "next/server";
 
 const client = new S3Client({
   region: process.env.AWS_REGION,
@@ -7,3 +8,27 @@ const client = new S3Client({
     secretAccessKey: process.env.SECRET_KEY,
   },
 });
+
+export async function POST(req) {
+  try {
+    const formData = await req.formData();
+    const file = formData.get("file");
+    console.log(file);
+
+    if (!file) {
+      NextResponse.json({ error: "no file uploaded" }, { status: 400 });
+    }
+
+    const fileName = `${Date.now()}-${file.name}`;
+
+    const command = new PutObjectCommand({
+      Bucket: process.env.S3_BUCKET,
+      Key: `uploads/${fileName}`,
+      ContentType: file.type,
+    });
+
+    return NextResponse.json({ message: "ok" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
