8de5b96f47269efd78cd3340d20c1a6667fe11ea
zaid <h.zaidamjad29@gmail.com>
2025-09-09 18:21:08 +0500

    get uploaded image url from s3

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -18,16 +18,23 @@ export async function POST(req) {
     if (!file) {
       NextResponse.json({ error: "no file uploaded" }, { status: 400 });
     }
-
+    const buffer = Buffer.from(await file.arrayBuffer());
     const fileName = `${Date.now()}-${file.name}`;
 
     const command = new PutObjectCommand({
       Bucket: process.env.S3_BUCKET,
       Key: `uploads/${fileName}`,
+      Body: buffer,
       ContentType: file.type,
     });
 
-    return NextResponse.json({ message: "ok" }, { status: 200 });
+    await client.send(command);
+
+    //https://zaid-private-bucket.s3.us-east-1.amazonaws.com/uploads/1757423724419-image2.jpeg
+
+    const imageUrl = `https://zaid-private-bucket.s3.us-east-1.amazonaws.com/uploads/${fileName}`
+
+    return NextResponse.json({ url: imageUrl }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error });
   }
